250f51e29838b1237703e6b16469bbd69c9a87d5
Denis Vashchenko <denisvashchenko@gmail.com>
2018-11-04 15:16:29 -0800

    working graphs

--- Scripts/GraphElement.cs
+++ Scripts/GraphElement.cs
@@ -10,52 +10,45 @@ public class GraphElement
     string graphTitle;
     string xAxisName;
     string yAxisName;
-    int xAxisMax;
-    int yAxisMax;
-    int numberOfXPoints;
-    float[] points; 
-    const int numOfPoints = 50;
+    float xAxisMax;
+    float yAxisMax;
+    public float[] points; 
+    int numOfPoints = 30;
     const int positionMin = 0;
     const int positionMax = 1500;
     const int heightMax = 1000;
     const int heightMin = 300;
     Random r = new Random();
     
-    public GraphElement(){
-        this.xAxisName = "Time";
-        this.yAxisName = "Value";
-        this.positionX = r.Next(positionMin, positionMax);
-        this.positionY = r.Next(positionMin, positionMax);
-        this.height = r.Next(heightMin, heightMax);
-        this.width = r.Next(heightMin, heightMax);
-        float[] points = new float[numOfPoints];
-    }
-
     public GraphElement(string graphTitle,string yName = "Value", string xName = "Time"){
-        this.xAxisName = xName;
-        this.yAxisName = yName;
-        this.graphTitle = graphTitle;   
-        this.positionX = r.Next(positionMin, positionMax);
-        this.positionY = r.Next(positionMin, positionMax);
-        this.height = r.Next(heightMin, heightMax);
-        this.width = r.Next(heightMin, heightMax);
-        float[] points = new float[numOfPoints];
+        numOfPoints = GraphingManager.MAX_NUMBER_POINTS;
+        this.positionX = r.Next(positionMin,positionMax);
+        this.positionY = r.Next(positionMin,positionMax);
+        this.width = r.Next(heightMin,heightMax);
+        this.height = r.Next(heightMin,heightMax);
+        this.xAxisName = "Time";
+        this.yAxisName = "Values";
+        this.graphTitle = graphTitle;  
+        
+        points = new float[numOfPoints];
+        for (int i = 0; i < numOfPoints; i++)
+        {
+            points[i] = 100f;                            
+        } 
     }
 
-    public GraphElement(string xName, string yName, int xMax, int yMax){
-        this.xAxisName = xName;
-        this.yAxisName = yName;
-        this.xAxisMax = xMax;
-        this.yAxisMax = yMax;
-        float[] points = new float[numOfPoints]; 
-    }
-
-    public float[] addPoint(int val){
-        points[numberOfXPoints++ % numOfPoints] = val;
-
-        if(val > yAxisMax) {
+    public float[] addPoint(float val){
+        for(int i = 0; i < numOfPoints - 1; i++)
+        {
+            points[i] = points[i+1];
+        }
+        points[numOfPoints - 1] = val;
+        
+        if(val > yAxisMax) 
+        {
             yAxisMax = val;             
-        }   
+        }
+
         return points;            
     }
 }
--- Scripts/GraphingManager.cs
+++ Scripts/GraphingManager.cs
@@ -6,33 +6,36 @@ using UnityEngine.UI;
 
 public class GraphingManager : MonoBehaviour {
 
-	int counter = 0;
+	int counter = 10;
 	string[] data;
 	//public string val = "chw_water";
 
-	private const short MAX_NUMBER_GRAPHS = 8;
+	private const short MAX_NUMBER_GRAPHS = 1;
+	public const short MAX_NUMBER_POINTS = 5;
 	GraphElement[] graphs;	
 	GameObject[] graphPrefab;
 	public GameObject graphTemplate;
+	public GameObject pointTemplate;
+	public GameObject lineTemplate;
 
 	//Time Variable Here
-	private const short CHW_TOTAL = 1;
-	private const short HW_TOTAL = 2;
-	private const short HW_KL = 3; 
-	private const short CHW_KL = 4;
-	private const short HW_COB = 5;
-	private const short CHW_COB = 6;
-	private const short HW_SE1 = 7;
-	private const short CHW_SE1 = 8;
-	private const short HW_SE2 = 9;
-	private const short CHW_SE2 = 10;
-	private const short HW_SSB = 11;
-	private const short CHW_SSB = 12;
-	private const short HW_SSM = 13;
-	private const short CHW_SSM = 14;
-	private const short HW_SAAC = 15;
-	private const short CHW_SAAC = 16;
-	private const short Total_Sunpower_Kwh = 17;
+	private const short CHW_TOTAL = 0;
+	private const short HW_TOTAL = 1;
+	private const short HW_KL = 2; 
+	private const short CHW_KL = 3;
+	private const short HW_COB = 4;
+	private const short CHW_COB = 5;
+	private const short HW_SE1 = 6;
+	private const short CHW_SE1 = 7;
+	private const short HW_SE2 = 8;
+	private const short CHW_SE2 = 9;
+	private const short HW_SSB = 10;
+	private const short CHW_SSB = 11;
+	private const short HW_SSM = 12;
+	private const short CHW_SSM = 13;
+	private const short HW_SAAC = 14;
+	private const short CHW_SAAC = 15;
+	private const short Total_Sunpower_Kwh = 16;
 
 
 	Vector2 mouseLocation;
@@ -54,12 +57,30 @@ public class GraphingManager : MonoBehaviour {
 			graphPrefab[graph] = Instantiate(graphTemplate) as GameObject; 
 			graphPrefab[graph].GetComponent<Transform>().parent = GameObject.Find("Canvas").GetComponent<Transform>();				
 			graphPrefab[graph].GetComponent<RectTransform>().position = new Vector2(graphs[graph].positionX, graphs[graph].positionY);
+			
+			graphPrefab[graph].GetComponent<NodeManager>().points = new GameObject[GraphingManager.MAX_NUMBER_POINTS];
+			graphPrefab[graph].GetComponent<NodeManager>().lines = new GameObject[GraphingManager.MAX_NUMBER_POINTS-1];
+			
+			for (int point = 0; point < MAX_NUMBER_POINTS; point++) {
+				GameObject pointCopy = Instantiate(pointTemplate) as GameObject; 		
+				pointCopy.GetComponent<Transform>().parent = graphPrefab[graph].GetComponent<Transform>(); 		
+				graphPrefab[graph].GetComponent<NodeManager>().points[point] = pointCopy;
+			}	
+			for (int point = 0; point < MAX_NUMBER_POINTS-1; point++) {
+				GameObject lineCopy = Instantiate(lineTemplate) as GameObject; 		
+				lineCopy.GetComponent<Transform>().parent = graphPrefab[graph].GetComponent<Transform>(); 		
+				graphPrefab[graph].GetComponent<NodeManager>().lines[point] = lineCopy;
+			}	
 		}
 	}
-	
+	int delay = 0;
 	// Update is called once per frame
 	void Update () 
 	{	
+		if (true)
+		{
+		
+		//DRAG START
 		if (Input.GetMouseButtonDown(0))
 		{
 			for (int graph = 0; graph < MAX_NUMBER_GRAPHS; graph++) {
@@ -72,13 +93,13 @@ public class GraphingManager : MonoBehaviour {
 				float yMin = graphs[graph].positionY;
 				float yMax = graphs[graph].positionY + graphs[graph].height;
 				
-                if (clickOn(xMin, xMax, yMin, yMax, mouseLocation, new Vector2(graphs[graph].positionX, graphs[graph].positionY) ))
+                if (clickOn(-graphs[graph].width/2, graphs[graph].width/2, -graphs[graph].height/2, graphs[graph].height/2, mouseLocation, new Vector2(graphs[graph].positionX, graphs[graph].positionY)))
             	{
 					graphs[graph].over = true;	 
 				}                   
 			}
 		}
-
+		//DRAG MOVE
 		if (Input.GetMouseButton(0))
         {
 			Vector2 newMouseLocation = Input.mousePosition;		
@@ -89,24 +110,87 @@ public class GraphingManager : MonoBehaviour {
                 {
                    	graphs[graph].positionX += -(mouseLocation.x - newMouseLocation.x);
 					graphs[graph].positionY += -(mouseLocation.y - newMouseLocation.y);
+					print ( graphs[graph].positionX);
+					print ( graphs[graph].positionY);
                 }      
 			}       
             mouseLocation = newMouseLocation;
         }
-			
+		//FORCE UPDATE
 		for (int graph = 0; graph < MAX_NUMBER_GRAPHS; graph++) {
-			graphPrefab[graph].GetComponent<RectTransform>().position = new Vector2(graphs[graph].positionX, graphs[graph].positionY);
-		 	graphPrefab[graph].GetComponent<RectTransform>().position = new Vector2(graphs[graph].width, graphs[graph].height);
+			
+			/*
+			
+				push csv data
+			 */			
+			//Checks if it's the sun power									//if graph == 0; index == 4
+			if(graph == 8)
+			{
+				//SunPower
+				//print(float.Parse(data[counter++].Split(',')[getIndexOf(graph)]));
+				float val = 0;
+				if (float.TryParse(data[counter++].Split(',')[getIndexOf(graph)], out val)) 
+				{
+					graphs[graph].addPoint(val);				
+							
+				}
+					else { graphs[graph].addPoint(0f);}		
+			}
+			else{
+				float val = 0;
+				if (float.TryParse(data[counter++].Split(',')[getIndexOf(graph)], out val)) 
+				{
+					graphs[graph].addPoint(val);				
+							
+				}		
+				else { graphs[graph].addPoint(0f);}		
+			}
+			
 
+			graphPrefab[graph].GetComponent<RectTransform>().position = new Vector2(graphs[graph].positionX, graphs[graph].positionY);
+		 	graphPrefab[graph].GetComponent<RectTransform>().sizeDelta = new Vector2(graphs[graph].width, graphs[graph].height);
+			//UPDATE NODES
+			for (int point = 0; point < MAX_NUMBER_POINTS; point++) {
+				Vector2 new_pos = new Vector2((float)point/(float)MAX_NUMBER_POINTS * (float)graphs[graph].width - graphs[graph].width / 2, graphs[graph].points[point]);
+				graphPrefab[graph].GetComponent<NodeManager>().points[point].GetComponent<RectTransform>().localPosition = new_pos;
+			}	
+			//UPDATE LINES
+			for (int point = 0; point < MAX_NUMBER_POINTS-1; point++) { 		
+			//distance and theta			 
+				//define x of points[point](float)point/(float)MAX_NUMBER_POINTS * (float)graphs[graph].width
+				float nodePointX = (float)point/(float)MAX_NUMBER_POINTS * (float)graphs[graph].width;
+				float nodePointX1 = ((float)point+1)/(float)MAX_NUMBER_POINTS * (float)graphs[graph].width;
+				float distance = Mathf.Sqrt(Mathf.Pow(nodePointX -nodePointX1,2) + Mathf.Pow(graphs[graph].points[point]-graphs[graph].points[point+1],2));  
+				float angle = Mathf.Rad2Deg*Mathf.Atan((graphs[graph].points[point]-graphs[graph].points[point+1])/(nodePointX-nodePointX1));
+				float xNew = (nodePointX +nodePointX1) / 2 - graphs[graph].width / 2;
+				float yNew = (graphs[graph].points[point] +graphs[graph].points[point+1]) / 2;
+
+
+				graphPrefab[graph].GetComponent<NodeManager>().lines[point].GetComponent<RectTransform>().sizeDelta = new Vector2(distance, 10);
+				graphPrefab[graph].GetComponent<NodeManager>().lines[point].GetComponent<RectTransform>().localRotation = Quaternion.Euler(new Vector3(0,0,angle));	
+				graphPrefab[graph].GetComponent<NodeManager>().lines[point].GetComponent<RectTransform>().localPosition = new Vector2(xNew, yNew);
+
+			}			
+		}
 		}
 		
 	}
-    //CHECKS IF CLICK IS WITHIN GIVEN BOUNDS
+	
+
+	//CHECKS IF CLICK IS WITHIN GIVEN BOUNDS
     public bool clickOn(float xMin, float xMax, float yMin, float yMax, Vector2 mousePosition, Vector2 otherPosition)
     {
         if (mousePosition.x + xMin < otherPosition.x && mousePosition.x + xMax > otherPosition.x) if (mousePosition.y + yMin < otherPosition.y && mousePosition.y + yMax > otherPosition.y) return true;
         return false;
     }
+    // //CHECKS IF CLICK IS WITHIN GIVEN BOUNDS
+    // public bool clickOn(float xMin, float xMax, float yMin, float yMax, Vector2 mousePosition)
+    // {
+    //     if (mousePosition.x > xMin  && mousePosition.x <  xMax)
+	// 	 if (mousePosition.y > yMin && mousePosition.y < yMax) 
+	// 	 	return true;
+    //     return false;
+    // }
 
 	   public string getNameOf(int value){
         switch (value)
@@ -132,5 +216,30 @@ public class GraphingManager : MonoBehaviour {
         }
 		return "";
     }
+
+	public int getIndexOf(int value){
+		 switch (value)
+        {
+            case 0:
+                return 1;
+            case 1:
+                return 3;
+            case 2:
+                return 5;
+            case 3:
+                return 7;
+            case 4:
+                return 9;
+            case 5:
+                return 11;
+            case 6:
+                return 13;
+            case 7:
+                return 15;
+            case 8:
+                return 17;
+        }
+		return -1;
+	}
 	
 }
